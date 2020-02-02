cdc5d241748ed1925d446fdd19498a68d70e7639
paynedeng <payne.d1991@gmail.com>
2020-02-02 22:54:49 +0800

    modify

--- src/main.js
+++ src/main.js
@@ -1,3 +1,11 @@
-const x = jQuery(".test");
+const $div1 = $('<div>1</div>')
+$div.appendTo(document.body)
 
-x.children().print();
+// div2.appendChild()
+// div2 到底是 DOM 对象 还是 jQuery 对象
+// DOM 对象只能使用 DOM API querySelector appendChild
+// jQuery 对象只能使用 jQuery 的 API find each
+// elDiv1 or div1
+// $div2
+
+//默认$div大部分时候对应了多个div是个数组，需要遍历它
